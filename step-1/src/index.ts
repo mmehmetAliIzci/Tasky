a49ca11fb919967453f2cf7eaaf609b5fda7d49c
Mehmet Ali IZCI <mmehmetaliizci@hotmail.com>
2023-08-24 11:41:00 +0200

    feat: initilize repo

---
+++ step-1/src/index.ts
@@ -0,0 +1,14 @@
+import { App } from '@slack/bolt';
+
+// Initializes your app with your bot token and signing secret
+const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+(async () => {
+    // Start your app
+    await app.start(process.env.PORT || 3000);
+
+    console.log('⚡️ Bolt app is running!');
+})();
