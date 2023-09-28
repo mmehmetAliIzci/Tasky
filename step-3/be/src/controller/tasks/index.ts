08dfde2852111197f917f2abb3083fbab8cce220
Mehmet Ali IZCI <mmehmetaliizci@hotmail.com>
2023-09-28 14:31:46 +0200

    feat: init Golang

--- step-3/be/src/controller/tasks/index.ts
+++
@@ -1,7 +0,0 @@
-import { addAssignee } from "./addAssignee";
-import { addTask } from "./addTask";
-import { getNextAssignee } from "./getNextAssignee";
-import { removeAssignee } from "./removeAssignee";
-import { removeTask } from "./removeTask";
-
-export { addTask, addAssignee, getNextAssignee, removeAssignee, removeTask };
